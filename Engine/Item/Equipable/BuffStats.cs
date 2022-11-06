fdc385c58acd9f8afc590dd97510c08de84467c0
hellun205 <hellun515@gmail.com>
2022-11-06 21:54:21 +0900

    player stats

--- Engine/Item/Equipable/BuffStats.cs
+++ Engine/Item/Equipable/BuffStats.cs
@@ -4,9 +4,23 @@ namespace goguma_v2.Engine.Item.Equipable;
 
 public struct BuffStats
 {
+  private Byte _avoidanceRate;
+  private ushort _receiveExpGrowthRate;
+  
+  
   public int MaxHp { get; set; }
   
-  // TO DO
-  public Byte AvoidanceRate { get; set; } // 회피율
+  // to do
+  public Byte AvoidanceRate
+  {
+    get => _avoidanceRate;
+    set => Util.Limit(0, 100, value);
+  }
+
+  public float ReceiveExpGrowthRate
+  {
+    get => _receiveExpGrowthRate;
+    set => Util.LimitF(0f, 10f, Math.Round(value, 1));
+  }
   
 }
--- Engine/Player/Player.cs
+++ Engine/Player/Player.cs
@@ -6,59 +6,24 @@ namespace goguma_v2.Engine.Player;
 public partial class Player
 {
   public string Name { get; private set; }
-  public ushort Level { get; private set; } = 1;
-  public uint Exp { get; private set; } = 0;
-  public uint Hp { get; private set; } = 50;
-  public uint MaxHp { get; private set; } = 50;
   public string Class { get; private set; } = "없음";
   public Inventory.Inventory Inventory { get; set; }
+  public Stats Stats { get; set; }
 
-  public static uint GetNextLevelExp(ushort level) =>
-    (uint) Math.Floor(0.04 * (level ^ 3) + 0.8 * (level ^ 2) + 2 * level);
 
   public Player(string name)
   {
     Name = name;
     Inventory = new(new[] {"장비", "소비", "기타"});
-  }
-
-  public void GainExp(uint amount)
-  {
-    uint leftAmount;
-    if (Exp + amount >= GetNextLevelExp(Level))
+    Stats = new Stats()
     {
-      leftAmount = Exp + amount - GetNextLevelExp(Level);
-      Level += 1;
-      GainExp(leftAmount);
-    }
-    else
-    {
-      Exp += amount;
-    }
+      MaxHp = 50,
+      Hp = 50,
+      AvoidanceRate = 0,
+      ReceiveExpGrowthRate = 0,
+    };
   }
 
-  public void GainHp(uint amount) => Hp = Math.Min(MaxHp, Hp + amount);
 
-  public void LoseHp(uint amount)
-  {
-    if (Hp < amount)
-      Hp = 0;
-    else
-      Hp -= amount;
-  }
-
-  public void GainMaxHp(uint amount) => MaxHp += amount;
-
-  public void LoseMaxHp(uint amount)
-  {
-    if (MaxHp < amount)
-    {
-      MaxHp = 1;
-      Hp = Math.Min(Hp, MaxHp);
-    }
-    else
-      MaxHp -= amount;
-  }
-
-  public PlayerData GetData() => new PlayerData() {Name = this.Name, Class = this.Class, Level = this.Level};
+  public PlayerData GetData() => new PlayerData() {Name = this.Name, Class = this.Class, Level = Stats.Level};
 }
