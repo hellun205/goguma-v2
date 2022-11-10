942eba32508e34c9134e2ddfa714c3474c0d961a
hellun205 <hellun515@gmail.com>
2022-11-10 22:17:59 +0900

    limit

--- Engine/Player/Stats.cs
+++ Engine/Player/Stats.cs
@@ -3,8 +3,7 @@ using System;
 namespace GogumaV2.Engine.Player;
 
 [Serializable]
-[Obsolete("need to fix this code and need limited value class")]
-public sealed class Stats 
+public sealed class Stats
 {
   public static uint GetNextLevelExp(ushort level) =>
     (uint) Math.Floor(0.04 * (level ^ 3) + 0.8 * (level ^ 2) + 2 * level);
@@ -26,79 +25,79 @@ public sealed class Stats
   public uint MaxHp // 최대 HP
   {
     get => _maxHp;
-    set => _maxHp = (uint) Util.Limit(1, uint.MaxValue, value);
+    set => _maxHp = value.Limit(1, uint.MaxValue);
   }
 
   public uint Hp // HP
   {
     get => _hp;
-    set => _hp = (uint) Util.Limit(0, MaxHp, value);
+    set => _hp = value.Limit(0, MaxHp);
   }
 
   public uint MaxMoisture // 최대 MP
   {
     get => _maxMoisture;
-    set => _maxMoisture = (uint) Util.Limit(1, uint.MaxValue, value);
+    set => _maxMoisture = value.Limit(1, uint.MaxValue);
   }
 
   public uint Moisture // MP
   {
     get => _moisture;
-    set => _moisture = (uint) Util.Limit(0, MaxHp, value);
+    set => _moisture = value.Limit(0, MaxHp);
   }
 
   public ushort Level // 레벨
   {
     get => _lv;
-    private set => _lv = (ushort) Util.Limit(1, ushort.MaxValue, value);
+    private set => _lv = value.Limit(1, ushort.MaxValue);
   }
 
   public uint Exp // 경험치
   {
     get => _exp;
-    private set => _exp = (uint) Util.Limit(0, uint.MaxValue, value);
+    private set => _exp = value;
   }
 
   public byte AvoidanceRate // 회피율
   {
     get => _avoidanceRate;
-    set => _avoidanceRate = (byte) Util.Limit(0, 100, value);
+    set => _avoidanceRate = value.Limit(0, 100);
   }
 
   public double ReceiveExpGrowthRate // 경험치 획득량 증가
   {
     get => _receiveExpGrowthRate;
-    set => _receiveExpGrowthRate = Util.LimitF(0d, 10d, Math.Round(value, 1));
+    set => _receiveExpGrowthRate = Math.Round(value, 1).Limit(0, 10);
   }
 
   public short Armor // 방어력 = (+) 1 / (1 + ARMOR * 0.01) / (-) 2 - (1 / (1 - ARMOR * 0.01))
   {
     get => _armor;
-    set => _armor = (short) Util.Limit(-400, 400, value);
+    set => _armor = value.Limit(-400,400);
   }
 
   public short CritArmor // 치명타 방어력 = (+) 1 / (1 + ARMOR * 0.01) / (-) 2 - (1 / (1 - ARMOR * 0.01))
   {
     get => _critArmor;
-    set => _critArmor = (short) Util.Limit(-400, 400, value);
+    set => _critArmor = value.Limit(-400,400);
   }
-  
+
   public byte CritPer // 치명타 확률
   {
     get => _critPer;
-    set => _critPer = (byte) Util.Limit(0, 100, value);
+    set => _critPer = value.Limit(0, 100);
   }
-  
+
   public byte CritDmg // 치명타 데미지 ( % ) = DMG * (1 + CRITDMG / 100)
   {
     get => _critDmg;
-    set => _critDmg = (byte) Util.Limit(0, 100, value);
+    set => _critDmg = value.Limit(0, 100);
   }
-  
+
   public ushort ArmorPenetration // 방어력 관통력 = ARMOR - PENETRATION
   {
     get => _armorPenetration;
-    set => _armorPenetration = (ushort) Util.Limit(0, 400, value);
+    set => _armorPenetration = value.Limit(0, 400);
   }
 
   public void GainExp(uint amount)
@@ -130,6 +129,5 @@ public sealed class Stats
     CritDmg = 0;
     Level = 1;
     ArmorPenetration = 0;
-
   }
 }
--- Util.cs
+++ Util.cs
@@ -4,18 +4,15 @@ namespace GogumaV2;
 
 public static class Util
 {
-  public static decimal Limit(decimal min, decimal max, decimal value)
-  {
-    return Math.Max(min, Math.Min(max, value));
-  }
-  
-  public static float LimitF(float min, float max, float value)
-  {
-    return Math.Max(min, Math.Min(max, value));
-  }
-  
-  public static double LimitF(double min, double max, double value)
-  {
-    return Math.Max(min, Math.Min(max, value));
-  }
+  public static int Limit(this int value, int min, int max) => (int)Math.Max(min, Math.Min(max, value));
+  public static uint Limit(this uint value, uint min, uint max) => (uint)Math.Max(min, Math.Min(max, value));
+  public static short Limit(this short value, short min, short max) => (short)Math.Max(min, Math.Min(max, value));
+  public static ushort Limit(this ushort value, ushort min, ushort max) => (ushort)Math.Max(min, Math.Min(max, value));
+  public static byte Limit(this byte value, byte min, byte max) => (byte)Math.Max(min, Math.Min(max, value));
+  public static sbyte Limit(this sbyte value, sbyte min, sbyte max) => (sbyte)Math.Max(min, Math.Min(max, value));
+  public static decimal Limit(this decimal value, decimal min, decimal max) => Math.Max(min, Math.Min(max, value));
+  public static long Limit(this long value, long min, long max) => Math.Max(min, Math.Min(max, value));
+  public static ulong Limit(this ulong value, ulong min, ulong max) => Math.Max(min, Math.Min(max, value));
+  public static double Limit(this double value, double min, double max) => Math.Max(min, Math.Min(max, value));
+  public static float Limit(this float value, float min, float max) => Math.Max(min, Math.Min(max, value));
 }
