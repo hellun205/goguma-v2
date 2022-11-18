3f6fd4a9f1fd92a727d160cd2b37ee4fe082682e
hellun205 <hellun515@gmail.com>
2022-11-18 18:54:38 +0900

    get drop item

--- GogumaWPF/Engine/Entity/EntityExtensions.cs
+++ GogumaWPF/Engine/Entity/EntityExtensions.cs
@@ -1,9 +1,25 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using GogumaWPF.Engine.Entity.Dialog;
 
 namespace GogumaWPF.Engine.Entity;
 
 public static class EntityExtensions
 {
+  public static string[] GetDrops(this IEnumerable<DropItem> dropItems)
+  {
+    List<string> resList = new();
+    
+    foreach (var dropItem in dropItems)
+    {
+      if (dropItem.Chance > 0)
+      {
+        var rand = new Random();
+        if (dropItem.Chance >= rand.Next(1, 101))
+          resList.Add(dropItem.ItemCode);
+      }
+    }
 
+    return resList.ToArray();
+  }
 }
---
+++ GogumaWPF/Goguma/Entity/TestNPC2.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Windows.Media;
+using GogumaWPF.Engine.Entity;
+using GogumaWPF.Engine.Entity.Dialog;
+
+namespace GogumaWPF.Goguma.Entity;
+
+public class TestNPC2 : Engine.Entity.Entity, IEnemy, IDropable
+{
+  public TestNPC2(string code) : base(code)
+  {
+    
+  }
+  
+  public EntityStatus Status { get; }
+  
+  public IEnumerable<DropItem> DropItems { get; }
+}
